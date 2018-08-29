c577a281fc715ba21831be4ca73a2727ba3bbe52
Jimmy <jwdepetro@depetro-macbook.lan>
2018-08-28 20:50:55 -0600

    improved validation for dup username

--- app/forms.py
+++ app/forms.py
@@ -31,6 +31,16 @@ class ProfileForm(FlaskForm):
     about_me = TextAreaField('About Me', validators=[Length(min=0, max=140)])
     submit = SubmitField('Save')
 
+    def __init__(self, original_username, *args, **kwargs):
+        super(ProfileForm, self).__init__(*args, **kwargs)
+        self.original_username = original_username
+    
+    def validate_username(self, username):
+        if username.data != self.original_username:
+            user = User.query.filter_by(username=self.username.data).first()
+            if user is not None:
+                raise ValidationError('Please use a different username.')
+
 class RecordForm(FlaskForm):
     artist = StringField('Artist', validators=[DataRequired()])
     album = StringField('Album', validators=[DataRequired()])
