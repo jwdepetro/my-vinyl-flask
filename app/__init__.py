5cdb742b3c31be0d33a8a5fceb5f18f2930760d8
Jimmy <jwdepetro@depetro-macbook.lan>
2018-08-28 22:16:55 -0600

    adding moment js

--- app/__init__.py
+++ app/__init__.py
@@ -4,6 +4,7 @@ from flask import Flask
 from config import Config
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
+from flask_moment import Moment
 from flask_login import LoginManager
 from logging.handlers import SMTPHandler, RotatingFileHandler
 
@@ -11,6 +12,7 @@ app = Flask(__name__)
 app.config.from_object(Config)
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
+moment = Moment(app)
 login = LoginManager(app)
 login.login_view = 'login'
 
