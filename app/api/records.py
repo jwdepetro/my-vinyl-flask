5420cffcb69bfa7c2819fed6645657ee9c500d36
Jimmy <jwdepetro@macbooks-mbp.lan>
2018-10-30 20:36:33 -0600

    created my records api endpoint

--- app/api/records.py
+++ app/api/records.py
@@ -1,8 +1,8 @@
-from flask import jsonify, request
-
+from flask import jsonify, request, abort
+from flask_login import current_user
 from app.api import bp
 from app.api.auth import token_auth
-from app.models import Record
+from app.models import Record, User
 
 
 @bp.route('/records', methods=['GET'])
@@ -10,7 +10,21 @@ from app.models import Record
 def get_records():
     page = request.args.get('page', 1, type=int)
     per_page = min(request.args.get('per_page', 10, type=int), 100)
-    data = Record.to_collection_dict(Record.query, page, per_page, 'api.get_records')
+    data = Record.to_collection_dict(
+        Record.query, page, per_page, 'api.get_records')
+    return jsonify(data)
+
+
+@bp.route('/records/mine', methods=['GET'])
+@token_auth.login_required
+def get_my_records():
+    user = User.check_token(request.headers.get('Authorization').split(" ")[1])
+    if not user:
+        abort(401)
+    page = request.args.get('page', 1, type=int)
+    per_page = min(request.args.get('per_page', 10, type=int), 100)
+    data = Record.to_collection_dict(Record.query.filter_by(
+        user_id=user.id), page, per_page, 'api.get_my_records')
     return jsonify(data)
 
 
